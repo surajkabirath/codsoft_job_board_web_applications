ca62b1b7810695e3d6fca1797b4f80c79ba71c02
surajkabirath <surajkabirath1234@gmail.com>
2024-06-11 14:18:11 +0545

    frontend change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
-
-
 const App = () => {
   return (
-    <div className="w-full flex justify-center">Job board</div>
-  )
-}
+    <div className="w-full flex flex-col  bg-red-500 h-16 ">
+      <div className="max-w-screen-xl bg-red-500 h-10 flex flex-col w-full mx-auto">
+        <h1 className="text-white font-bold">HEllo WOERLD</h1>
+        <p className="sm:hidden inline-block">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam
+          aliquid eaque tempore iusto enim? Cum aliquid alias cumque, beatae
+          voluptatum in aperiam quia.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
