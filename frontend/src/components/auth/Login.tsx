a758c742bb79e9c4ca7a987fb53735b9e80d9750
surajkabirath <surajkabirath1234@gmail.com>
2024-06-13 20:51:23 +0545

    login page create

---
+++ frontend/src/components/auth/Login.tsx
@@ -0,0 +1,67 @@
+const Login = () => {
+  return (
+    <section className="bg-gray-50 ">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mt-9 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+              LogIn here
+            </h1>
+            <form className="space-y-4 md:space-y-6" action="#">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 "
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  placeholder="name@company.com"
+                  required={true}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 "
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  required={true}
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="w-full text-black bg-gray-300 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+              >
+                Login
+              </button>
+              <p className="text-sm font-light text-gray-500 ">
+                Create a new Account?{" "}
+                <a
+                  href="/register"
+                  className="font-medium text-primary-600 hover:underline "
+                >
+                  Register here here
+                </a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
--- frontend/src/components/auth/Register.tsx
+++ frontend/src/components/auth/Register.tsx
@@ -103,7 +103,7 @@ const Register = () => {
               <p className="text-sm font-light text-gray-500 ">
                 Already have an account?{" "}
                 <a
-                  href="#"
+                  href="/login"
                   className="font-medium text-primary-600 hover:underline "
                 >
                   Login here
