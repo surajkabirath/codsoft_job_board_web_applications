6a466461b72ce7eff22d3fe64cc8db4d85b41c7f
surajkabirath <surajkabirath1234@gmail.com>
2024-06-13 21:04:33 +0545

    change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
+import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Navbar from "./components/layout/Navbar";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 const App = () => {
   return (
     <>
-    <Navbar/>
-    <Register/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
