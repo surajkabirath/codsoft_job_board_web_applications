8be621b6761e4711d29f8c87162cdafc956ba926
surajkabirath <surajkabirath1234@gmail.com>
2024-06-20 23:05:37 +0545

    add toast

--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -1,6 +1,41 @@
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import { Context } from "../../context/ContextProvider";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { isAuthorized, setIsAuthorized } = useContext(Context);
+
+  const userLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:8000/api/auth/login",
+        { email, password },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      toast.success(data.message);
+
+      setEmail("");
+      setPassword("");
+
+      setIsAuthorized(true);
+    } catch (error) {
+      const err = error as { response: { data: { message: string } } };
+      toast.error(err.response.data.message);
+    }
+    if (isAuthorized) {
+      return <Navigate to={"/"} />;
+    }
+  };
   return (
     <section className="bg-gray-50 ">
       <div className="flex flex-col items-center justify-center px-6 py-8 mt-9 mx-auto md:h-screen lg:py-0">
@@ -9,7 +44,11 @@ const Login = () => {
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
               LogIn here
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form
+              className="space-y-4 md:space-y-6"
+              action="#"
+              onSubmit={userLogin}
+            >
               <div>
                 <label
                   htmlFor="email"
@@ -20,10 +59,10 @@ const Login = () => {
                 <input
                   type="email"
                   name="email"
-                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                   placeholder="name@company.com"
-                  required={true}
                 />
               </div>
               <div>
@@ -36,10 +75,10 @@ const Login = () => {
                 <input
                   type="password"
                   name="password"
-                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                  required={true}
                 />
               </div>
 
